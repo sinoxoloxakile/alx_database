ed4f93e2a8e4dec776fd180c82558ad268b8fdf7
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 14:31:10 +0200

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- script that creates the first_table 
+-- if it doesn't exist in the specified database:
+
+CREATE TABLE IF NOT EXISTS `first_table`
+ (
+    `id` INT,
+    `name` VARCHAR(256)
+);
