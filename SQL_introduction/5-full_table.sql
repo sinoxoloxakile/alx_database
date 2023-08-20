a63b19e5c2b5bd0932ca2348cc64c29a271e1a6d
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 14:32:31 +0200

    Full description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- script that retrieves the full description of the first_table 
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
